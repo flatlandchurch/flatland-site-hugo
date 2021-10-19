e77fe3c4f8aaf883bff942ee205eb21c6fc72927
Matt McElwee <mmcelwee@dmsi.com>
2021-10-19 09:23:07 -0500

    Fixed a build issue

---
+++ scripts/sendForm.js
@@ -0,0 +1,10 @@
+export default function sendForm(formID, body) {
+  const baseURL = `https://flatland-forms-api-auevpolm5q-uc.a.run.app/forms/${formID}`;
+  return fetch(baseURL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).then((d) => d.json());
+}
