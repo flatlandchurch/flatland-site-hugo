b1b3eece0e42ce3651aaf37435551366d08e926d
Matt McElwee <mmcelwee@dmsi.com>
2021-10-19 09:35:44 -0500

    Added additional logging

--- functions/forms.js
+++ functions/forms.js
@@ -1,16 +1,17 @@
 const got = require('got');
+const catchify = require('catchify');
 
 const PCO_TOKEN = process.env.PCO_TOKEN;
 const PCO_SECRET = process.env.PCO_SECRET;
 
-exports.handler = async function (event, ctx, cb) {
+exports.handler = async function (event, ctx) {
   if (!event.queryStringParameters.id) {
-    return cb(null, {
+    return Promise.resolve({
       body: JSON.stringify({ errors: ['ID of form must be supplied.'] }),
     });
   }
 
-  const form = await got(
+  const [err, form] = await catchify(got(
     `https://api.planningcenteronline.com/people/v2/forms/${event.queryStringParameters.id}/fields`,
     {
       username: PCO_TOKEN,
@@ -21,9 +22,15 @@ exports.handler = async function (event, ctx, cb) {
           'options,field_definitions,field_options,marital_statuses,campuses,form_field_conditions,school_options',
       },
     },
-  ).json();
+  ).json());
 
-  cb(null, {
+  if (err) {
+    return Promise.resolve({
+      body: JSON.stringify({ errors: [err] }),
+    });
+  }
+
+  return Promise.resolve({
     body: JSON.stringify(form),
     headers: {
       'Access-Control-Allow-Origin': '*',
